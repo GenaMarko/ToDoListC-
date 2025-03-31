e380a246010f622cccc183b44e1d725ee6f80ce2
Gena Marko <genamarko03@gmail.com>
2025-03-31 14:38:54 +0300

    Added "select list" with options in "Index" page.

--- obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d71d048fef26ce4a5228c9ac90ddce1efbcda13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5eb870852d42b2ee472fc7f5e361e343a65b62f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

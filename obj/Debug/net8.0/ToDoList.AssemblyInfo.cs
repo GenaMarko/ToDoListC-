419ba34b414b3c54e271a982fe052785a293900c
Gena Marko <genamarko03@gmail.com>
2025-04-02 23:39:48 +0300

    Dancing with buben and front-end(Small fixes and stylization).

--- obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a627767e91c5fadce7af4caf7941596a69e3b045")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71de8051dac8018414943810dfd7017a145920dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
